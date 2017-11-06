ef40f6a809fb79ab4b1946742207e33ec1565a7e
boryita <boryita21@gmail.com>
2017-11-06 19:59:46 +0100

    paso2:funcion suma array okey

--- main.js
+++ main.js
@@ -13,7 +13,27 @@
 
 function SumaArray(numeros){
     let suma= 0;
-
+    for(let valor of numeros){
+        suma=suma+valor
+    }
     return suma;
+}
 
-}  
+function SumaArray2(numeros){
+    let suma=0
+    for(let pos in numeros){
+        suma=suma+numeros[pos];
+    }
+    return suma
+}
+
+function SumaArray3(numeros){
+    let suma=0
+    for(let i=0;i<numeros.length;i++){
+        suma=suma+numeros[i];
+    }
+    return suma;
+}
+    
+let sumaValor=SumaArray3([2,3,4,5])
+console.log(sumaValor);
