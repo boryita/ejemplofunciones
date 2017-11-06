78b82311e571b8f75b8b9f302e8c2423eea492e6
boryita <boryita21@gmail.com>
2017-11-06 19:34:15 +0100

    paso 1

---
+++ main.js
@@ -0,0 +1,19 @@
+"use strict"
+/**
+ * @file Este archivo contiene ejemplos de funciones con for
+ * @author Borja Fariña Gonzalez    
+ * @version 1.0
+ */
+
+ /**
+  * @description sumaArray Suma los valores de un array
+  * @param {Array} numeros array de numeros
+  * @return {Number}
+  */
+
+function SumaArray(numeros){
+    let suma= 0;
+
+    return suma;
+
+}  
